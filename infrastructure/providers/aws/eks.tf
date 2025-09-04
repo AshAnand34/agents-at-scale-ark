5a4cbc6a482c9bc4b41bf4fcb6b3e71ec911b4f1
Nadja Stark <Nadja_Stark@mckinsey.com>
2025-09-04 15:50:15 +0200

    Remove additional role granting to EKS access for github

--- infrastructure/providers/aws/eks.tf
+++ infrastructure/providers/aws/eks.tf
@@ -19,18 +19,3 @@ module "eks" {
 
   tags = local.tags
 }
-
-resource "aws_eks_access_entry" "github_access" {
-  cluster_name  = module.eks.cluster_name
-  principal_arn = data.aws_iam_role.github_oidc_role.arn
-}
-
-resource "aws_eks_access_policy_association" "github_access_policy" {
-  cluster_name  = module.eks.cluster_name
-  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
-  principal_arn = data.aws_iam_role.github_oidc_role.arn
-
-  access_scope {
-    type = "cluster"
-  }
-}
